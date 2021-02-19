4833c6ac4ba9f2c83f62e1e5922801e7c8af4b26
gathaus <rizamertyagci@gmail.com>
2021-02-19 20:18:58 +0300

    Connected sorular and dropdowns to database

--- public/js/form-handling.js
+++ public/js/form-handling.js
@@ -1,41 +1,77 @@
 function fetchData() {
   fetch("http://localhost:8080/api/survey/getLiselerData")
     .then((response) => response.json())
-    .then((response) =>{
-
-      for(i=0;i<response.length;i++){
+    .then((response) => {
+      for (i = 0; i < response.length; i++) {
         var element = document.getElementById("dropdown1");
         var option = document.createElement("option");
-        option.text = "deneme";
+        option.text = response[i].isim;
         element.add(option);
       }
-      console.log(response)
-      })
+      // console.log(response);
+    })
     .catch((error) => {
       console.log(error);
     });
 
-  
-
   fetch("http://localhost:8080/api/survey/getMesleklerData")
     .then((response) => response.json())
-    .then((response) =>{
-
-      for(i=0;i<response.length;i++){
+    .then((response) => {
+      for (i = 0; i < response.length * 3; i++) {
         var element = document.getElementById("dropdown2");
         var option = document.createElement("option");
-        option.text = "deneme"+i;
+        option.text = response[i].isim;
         element.add(option);
       }
-      console.log(response)
-      })
+      // console.log(response);
+    })
     .catch((error) => {
       console.log(error);
     });
 
   fetch("http://localhost:8080/api/survey/getSorularData")
     .then((response) => response.json())
-    .then((response) => console.log(response))
+    .then((response) => {
+      // console.log(response);
+      let soruCount = 0;
+      let breakCheck = false;
+      console.log(response.length);
+      for (i = 1; i <= response.length / 4; i++) {
+        const tbody = document.getElementById("tbody" + i);
+        for (k = 0; k < 10; k++) {
+          const trow = tbody.getElementsByTagName("tr")[k];
+          // console.log("trow");
+          // console.log(k);
+          const soru = trow.getElementsByTagName("td")[2];
+          soru.innerHTML = soruCount + 1 + ". " + response[soruCount].Soru;
+          if (soruCount >= response.length - 1) {
+            breakCheck = true;
+            break;
+          }
+          soruCount++;
+          // console.log(
+          //   "Soru Count: " +
+          //     soruCount +
+          //     "i: " +
+          //     i +
+          //     " " +
+          //     "k: " +
+          //     k +
+          //     " " +
+          //     soru.innerHTML
+          // );
+        }
+        if (breakCheck == true) break;
+        //Geçerlilik maddesi için konuldu
+        if (soruCount + 1 == 31) {
+          // console.log("ÇALIŞTI")
+          const trow = tbody.getElementsByTagName("tr")[k];
+          const soru = trow.getElementsByTagName("td")[2];
+          soru.innerHTML = soruCount + 1 + ". " + response[soruCount].Soru;
+          soruCount++;
+        }
+      }
+    })
     .catch((error) => {
       console.log(error);
     });
