9c29ead9321c2bb9d1f7e2e8194abc07e2f0bda3
gathaus <rizamertyagci@gmail.com>
2021-03-09 01:31:12 +0300

    Fix cleardb

--- middlewares/database/connectDatabase.js
+++ middlewares/database/connectDatabase.js
@@ -2,12 +2,7 @@
 
 const mysql = require('mysql2');
 
-var con = mysql.createConnection({
-  host: "us-cdbr-east-03.cleardb.com",
-  user: "b7ef3f2ee807bf",
-  password: "99e6dce6",
-  database: "heroku_cab903e0bd5020d"
-});
+var con = mysql.createConnection("mysql://b7ef3f2ee807bf:99e6dce6@us-cdbr-east-03.cleardb.com/heroku_cab903e0bd5020d?reconnect=true");
 
 con.connect(function(err) {
   if (err) throw err;
