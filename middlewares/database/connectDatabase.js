d2395c984ba60a15db45236313419847eb8c7ff8
gathaus <rizamertyagci@gmail.com>
2021-03-15 14:25:44 +0300

    .

--- middlewares/database/connectDatabase.js
+++ middlewares/database/connectDatabase.js
@@ -1,13 +1,18 @@
-"use strict";
+'use strict';
 
-const mysql = require("mysql");
+const mysql = require('mysql');
 
-var con = mysql.createConnection(process.env.DATABASE_URL);
+var con = mysql.createConnection({
+  host: "us-cdbr-east-03.cleardb.com",
+  user: "b7ef3f2ee807bf",
+  password: "99e6dce6",
+  database: "heroku_cab903e0bd5020d"
+});
 
-con.connect(function (err) {
+con.connect(function(err) {
   if (err) throw err;
 
-  console.log("MySql bağlantısı gerçekleşti");
+  console.log("MySql bağlantısı gerçekleşti")
 });
 
 module.exports = con;
